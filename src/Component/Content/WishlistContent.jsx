749cdb974c9c57c7981126dfa13565d2e81a6867
FiajRahman <fiaj15-3077@diu.edu.bd>
2024-03-28 17:01:14 +0600

    wishlistContent page commit

--- src/Component/Content/WishlistContent.jsx
+++ src/Component/Content/WishlistContent.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+// import React from 'react';
 
-const WishlistContent = () => {
-    return (
-        <div>
-            <h1>nehal</h1>
-        </div>
-    );
-};
+// const WishlistContent = () => {
+//     return (
+//         <div>
+//             <h1>nehal</h1>
+//         </div>
+//     );
+// };
 
-export default WishlistContent;
+// export default WishlistContent;
