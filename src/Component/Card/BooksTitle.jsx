6dd070cc8e34a998bc62ced75f91340a93bb9257
FiajRahman <fiaj15-3077@diu.edu.bd>
2024-03-28 16:59:36 +0600

    Edit card

--- src/Component/Card/BooksTitle.jsx
+++ src/Component/Card/BooksTitle.jsx
@@ -1,11 +1,11 @@
-// import React from 'react';
+import React from 'react';
 
-// const BooksTitle = () => {
-//     return (
-//         <div>
-//             <h1 className='text-center mt-12 text-5xl font-bold '>Books</h1>
-//         </div>
-//     );
-// };
+const BooksTitle = () => {
+    return (
+        <div>
+            <h1 className='text-center mt-12 text-5xl font-bold '>Books</h1>
+        </div>
+    );
+};
 
-// export default BooksTitle;
+export default BooksTitle;
